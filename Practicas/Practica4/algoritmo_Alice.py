fd0f273395b35aa5fedfbfc86e6bbb6ebfdca979
alraro <alfonsoramosrojas@gmail.com>
2026-04-16 14:15:42 +0200

    Terminada parte A

--- Practicas/Practica4/algoritmo_Alice.py
+++ Practicas/Practica4/algoritmo_Alice.py
@@ -1,4 +1,4 @@
-from custom_lib import funciones_rsa, funciones_aes
+from custom_lib import funciones_rsa, funciones_aes, socket_class
 from custom_lib.socket_class import SOCKET_SIMPLE_TCP
 from Crypto.Hash import HMAC, SHA256
 from Crypto.Random import get_random_bytes
@@ -46,45 +46,118 @@ jStr = json.dumps(mensaje)
 
 # Cifro el json con K1
 
-cifrador = 
+aes_cifrado, nonce_aes = funciones_aes.iniciarAES_CTR_cifrado(K1)
+
+datos_cifrado = funciones_aes.cifrarAES_CTR(aes_cifrado, jStr.encode("utf-8"))
+
 
 # Aplico HMAC
 
+cifrador_hmac = HMAC.new(K2, digestmod=SHA256)
+cifrador_hmac.update(datos_cifrado)
+mac = cifrador_hmac.digest()
 
 # Envío el json cifrado junto con el nonce del AES CTR, y el mac del HMAC
 
+socketclient.enviar(datos_cifrado)
+socketclient.enviar(nonce_aes)
+socketclient.enviar(mac)
 
 #####################
 #####################
 
 # Recibo el mensaje, junto con el nonce del AES CTR, y el mac del HMAC
 
+datos_cifrado_recibido = socketclient.recibir()
+nonce_aes_recibido = socketclient.recibir()
+mac_recibido = socketclient.recibir()
 
 # Descifro el mensaje
 
+datos_descifrado = funciones_aes.descifrarAES_CTR(funciones_aes.iniciarAES_CTR_descifrado(K1, nonce_aes_recibido), datos_cifrado_recibido)
+
 # Verifico el mac
 
+verificador_hmac = HMAC.new(K2, digestmod=SHA256)
+verificador_hmac.update(datos_cifrado_recibido)
+
+try:
+    verificador_hmac.verify(mac_recibido)
+    print("El mensaje es auténtico")
+except ValueError:
+    print("El mensaje no es auténtico")
 
 # Visualizo la identidad del remitente y compruebo si los campos enviados son los mismo que los recibidos
 
+mensaje_recibido = json.loads(datos_descifrado.decode("utf-8"))
+print("Identidad remitente:", mensaje_recibido[0])
+nA_recibido = bytearray.fromhex(mensaje_recibido[2])
+
+if mensaje_recibido[0] == "Bob" and mensaje_recibido[1] == "Alice" and nA_recibido == nA:
+    print("Correcto")
+else:
+    print("Mal")
+
 #####################
 #####################
 
-# Intercambio de información NUMERO 1. Al utilizar K1, reutilizo el canal de comunicaciones aes_cifrado
+# Intercambio de información NUMERO 1. Envío "Hola Amigas"
 
-# Aplico HMAC
+mensaje1 = "Hola Amigas"
+aes_cifrado1, nonce_aes1 = funciones_aes.iniciarAES_CTR_cifrado(K1)
+datos_cifrado1 = funciones_aes.cifrarAES_CTR(aes_cifrado1, mensaje1.encode("utf-8"))
 
+cifrador_hmac1 = HMAC.new(K2, digestmod=SHA256)
+cifrador_hmac1.update(datos_cifrado1)
+mac1 = cifrador_hmac1.digest()
 
-# Envío el json cifrado junto con el nonce del AES CTR, y el mac del HMAC
+socketclient.enviar(datos_cifrado1)
+socketclient.enviar(nonce_aes1)
+socketclient.enviar(mac1)
 
+# Recibo respuesta de Bob
+datos_cifrado1_resp = socketclient.recibir()
+nonce_aes1_resp = socketclient.recibir()
+mac1_resp = socketclient.recibir()
 
-# Intercambio de información NUMERO 2. Al utilizar K1, reutilizo el canal de comunicaciones aes_cifrado
+datos_descifrado1_resp = funciones_aes.descifrarAES_CTR(funciones_aes.iniciarAES_CTR_descifrado(K1, nonce_aes1_resp), datos_cifrado1_resp)
+verificador_hmac1 = HMAC.new(K2, digestmod=SHA256)
+verificador_hmac1.update(datos_cifrado1_resp)
 
+try:
+    verificador_hmac1.verify(mac1_resp)
+    print("Intercambio 1 - Mensaje recibido:", datos_descifrado1_resp.decode("utf-8"))
+except ValueError:
+    print("Intercambio 1 - Error de autenticidad")
 
-# Aplico HMAC
+# Intercambio de información NUMERO 2. Envío "Hola Amigas" nuevamente
 
+mensaje2 = "Hola Amigas"
+aes_cifrado2, nonce_aes2 = funciones_aes.iniciarAES_CTR_cifrado(K1)
+datos_cifrado2 = funciones_aes.cifrarAES_CTR(aes_cifrado2, mensaje2.encode("utf-8"))
 
-# Envío el json cifrado junto con el nonce del AES CTR, y el mac del HMAC
+cifrador_hmac2 = HMAC.new(K2, digestmod=SHA256)
+cifrador_hmac2.update(datos_cifrado2)
+mac2 = cifrador_hmac2.digest()
+
+socketclient.enviar(datos_cifrado2)
+socketclient.enviar(nonce_aes2)
+socketclient.enviar(mac2)
+
+# Recibo respuesta de Bob
+datos_cifrado2_resp = socketclient.recibir()
+nonce_aes2_resp = socketclient.recibir()
+mac2_resp = socketclient.recibir()
+
+datos_descifrado2_resp = funciones_aes.descifrarAES_CTR(funciones_aes.iniciarAES_CTR_descifrado(K1, nonce_aes2_resp), datos_cifrado2_resp)
+verificador_hmac2 = HMAC.new(K2, digestmod=SHA256)
+verificador_hmac2.update(datos_cifrado2_resp)
+
+try:
+    verificador_hmac2.verify(mac2_resp)
+    print("Intercambio 2 - Mensaje recibido:", datos_descifrado2_resp.decode("utf-8"))
+except ValueError:
+    print("Intercambio 2 - Error de autenticidad")
 
 
 # Cierro el socket
--- Practicas/Practica4/algoritmo_Bob.py
+++ Practicas/Practica4/algoritmo_Bob.py
@@ -30,44 +30,125 @@ else:
 
 # Recibo el mensaje, junto con el nonce del AES CTR, y el mac del HMAC
 
+datos_cifrado = socketserver.recibir()
+nonce_aes = socketserver.recibir()
+mac = socketserver.recibir()
+
 # Descifro el mensaje
+
+aes_descifrado = funciones_aes.iniciarAES_CTR_descifrado(K1, nonce_aes)
+datos_descifrado = funciones_aes.descifrarAES_CTR(aes_descifrado, datos_cifrado)
 
 # Verifico el mac
 
+verificador_hmac = HMAC.new(K2, digestmod=SHA256)
+verificador_hmac.update(datos_cifrado)
+
+try:
+    verificador_hmac.verify(mac)
+    print("Mensaje auténtico")
+except ValueError:
+    print("Mensaje NO auténtico")
+
 # Visualizo la identidad del remitente
+
+mensaje_recibido = json.loads(datos_descifrado.decode("utf-8"))
+print("Identidad remitente:", mensaje_recibido[0])
+nA = bytearray.fromhex(mensaje_recibido[1])
 
 #####################
 #####################
 
 # Genero el json con el nombre de Bob, el de Alice y el nonce nA
 
+mensaje_respuesta = ["Bob", "Alice", nA.hex()]
+jStr_respuesta = json.dumps(mensaje_respuesta)
+
 # Cifro el json con K1
+
+aes_cifrado, nonce_aes_resp = funciones_aes.iniciarAES_CTR_cifrado(K1)
+datos_cifrado_resp = funciones_aes.cifrarAES_CTR(aes_cifrado, jStr_respuesta.encode("utf-8"))
 
 # Aplico HMAC
 
+cifrador_hmac_resp = HMAC.new(K2, digestmod=SHA256)
+cifrador_hmac_resp.update(datos_cifrado_resp)
+mac_resp = cifrador_hmac_resp.digest()
+
 # Envío el json cifrado junto con el nonce del AES CTR, y el mac del HMAC
+
+socketserver.enviar(datos_cifrado_resp)
+socketserver.enviar(nonce_aes_resp)
+socketserver.enviar(mac_resp)
 
 #####################
 #####################
 
 # Recibo el primer mensaje de Alice
 
+datos_cifrado1 = socketserver.recibir()
+nonce_aes1 = socketserver.recibir()
+mac1 = socketserver.recibir()
+
 # Descifro el mensaje
 
+aes_descifrado1 = funciones_aes.iniciarAES_CTR_descifrado(K1, nonce_aes1)
+datos_descifrado1 = funciones_aes.descifrarAES_CTR(aes_descifrado1, datos_cifrado1)
 
 # Verifico el mac
 
-# Muestro el mensaje
+verificador_hmac1 = HMAC.new(K2, digestmod=SHA256)
+verificador_hmac1.update(datos_cifrado1)
 
+try:
+    verificador_hmac1.verify(mac1)
+    print("Intercambio 1 - Mensaje recibido:", datos_descifrado1.decode("utf-8"))
+except ValueError:
+    print("Intercambio 1 - Error de autenticidad")
+
+# Envío respuesta
+mensaje1_resp = "Hola Amigos"
+aes_cifrado1_resp, nonce_aes1_resp = funciones_aes.iniciarAES_CTR_cifrado(K1)
+datos_cifrado1_resp = funciones_aes.cifrarAES_CTR(aes_cifrado1_resp, mensaje1_resp.encode("utf-8"))
+cifrador_hmac1_resp = HMAC.new(K2, digestmod=SHA256)
+cifrador_hmac1_resp.update(datos_cifrado1_resp)
+mac1_resp = cifrador_hmac1_resp.digest()
+socketserver.enviar(datos_cifrado1_resp)
+socketserver.enviar(nonce_aes1_resp)
+socketserver.enviar(mac1_resp)
 
 # Recibo el segundo mensaje de Alice
 
+datos_cifrado2 = socketserver.recibir()
+nonce_aes2 = socketserver.recibir()
+mac2 = socketserver.recibir()
+
 # Descifro el mensaje
 
+aes_descifrado2 = funciones_aes.iniciarAES_CTR_descifrado(K1, nonce_aes2)
+datos_descifrado2 = funciones_aes.descifrarAES_CTR(aes_descifrado2, datos_cifrado2)
 
 # Verifico el mac
 
-# Muestro el mensaje
+verificador_hmac2 = HMAC.new(K2, digestmod=SHA256)
+verificador_hmac2.update(datos_cifrado2)
+
+try:
+    verificador_hmac2.verify(mac2)
+    print("Intercambio 2 - Mensaje recibido:", datos_descifrado2.decode("utf-8"))
+except ValueError:
+    print("Intercambio 2 - Error de autenticidad")
+
+# Envío respuesta
+mensaje2_resp = "Hola Amigos"
+aes_cifrado2_resp, nonce_aes2_resp = funciones_aes.iniciarAES_CTR_cifrado(K1)
+datos_cifrado2_resp = funciones_aes.cifrarAES_CTR(aes_cifrado2_resp, mensaje2_resp.encode("utf-8"))
+cifrador_hmac2_resp = HMAC.new(K2, digestmod=SHA256)
+cifrador_hmac2_resp.update(datos_cifrado2_resp)
+mac2_resp = cifrador_hmac2_resp.digest()
+socketserver.enviar(datos_cifrado2_resp)
+socketserver.enviar(nonce_aes2_resp)
+socketserver.enviar(mac2_resp)
 
 
 # Cierro el socket
